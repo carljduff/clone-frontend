cb2a3941dab37f349f45f2e400614678864c1eda
Carl Jordan Duff <jordan.duff@student.awesomeinc.org>
2022-04-28 23:00:57 +0000

    event chat update

--- src/components/EventChat.js
+++ src/components/EventChat.js
@@ -2,16 +2,20 @@ import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 
+
 const EventChat = (props) => {
-  const [comment, setComment] = React.useState({
-    comment: ""
-  })
+  const [comment, setComment] = useState([]);
+  const [input, setInput] = useState('')
 
   const handleInput = (e) => {
-    setComment({comment: e.target.value})
+    setInput(e.target.value)
   }
 
-
+  const inputSubmitHandler = (e) => {
+    setComment(input)
+    console.log(comment)
+    setInput('')
+  }
 
   return (
     <div>
@@ -22,79 +26,32 @@ const EventChat = (props) => {
         centered
       >
         <Modal.Header closeButton>
+          {/* <Modal.Title id="contained-modal-title-vcenter">
+          Event Chat
+        </Modal.Title> */}
           Event Chat
         </Modal.Header>
         <Modal.Body>
           <p>
-           {comment.comment}
+           {comment}
           </p>
         </Modal.Body>
         {/* <Modal.Footer> */}
           <div className="comment">
             
                 <input
-                value = {comment.comment}
-                // onChange={handleInput}
+                value = {input}
+                onChange={handleInput}
                 className="comment-input"
                   type="text"
                   name="chat"
                   placeholder="Add your comment..."
                 />
-            <button onClick={handleInput} className="comment-btn">Add </button>
+            <button onClick={inputSubmitHandler} className="comment-btn">Add </button>
           </div>
         {/* </Modal.Footer> */}
       </Modal>
     </div>
-
-// const EventChat = (props) => {
-//   const [comment, setComment] = useState([]);
-//   const [input, setInput] = useState('')
-
-//   const handleInput = (e) => {
-//     setInput(e.target.value)
-//   }
-
-//   const inputSubmitHandler = (e) => {
-//     setComment(input)
-//     console.log(comment)
-//     setInput('')
-//   }
-
-//   return (
-//     <div>
-//       <Modal
-//         {...props}
-//         size="lg"
-//         aria-labelledby="contained-modal-title-vcenter"
-//         centered
-//       >
-//         <Modal.Header closeButton>
-//           {/* <Modal.Title id="contained-modal-title-vcenter">
-//           Event Chat
-//         </Modal.Title> */}
-//           Event Chat
-//         </Modal.Header>
-//         <Modal.Body>
-//           <p>
-//            {comment}
-//           </p>
-//         </Modal.Body>
-//         {/* <Modal.Footer> */}
-//           <div className="comment">
-            
-//                 <input
-//                 value = {input}
-//                 onChange={handleInput}
-//                 className="comment-input"
-//                   type="text"
-//                   name="chat"
-//                   placeholder="Add your comment..."
-//                 />
-//             <button onClick={inputSubmitHandler} className="comment-btn">Add </button>
-//           </div>
-//         {/* </Modal.Footer> */}
-//       </Modal>
-//     </div>
   );
 };
 
