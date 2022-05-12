977e15ff4570305cee328dca75e008d00178b1aa
Carl Jordan Duff <jordan.duff@student.awesomeinc.org>
2022-05-12 14:17:28 +0000

    created env file

--- src/services/auth.constants.js
+++ src/services/auth.constants.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://8000-carljduff-clonebackend-qzjqj4zemon.ws-us44.gitpod.io/";
+export const API_URL = process.env.REACT_APP_API_URL
 export const LOGIN_ENDPOINT = "login/"
 export const REGISTER_ENDPOINT = "signup/"
 export const REFRESH_ENDPOINT = "token/refresh/"
